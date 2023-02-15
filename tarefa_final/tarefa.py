59df21ef8a34e1318563a6212b43c8d445ef18e4
Pedro Cavalcante Oliveira <pedrocolrj@gmail.com>
2023-02-15 19:05:23 -0300

    Update tarefa.py
    primeira tentativa do 2

--- tarefa_final/tarefa.py
+++ tarefa_final/tarefa.py
@@ -9,3 +9,49 @@ def k_most_frequent(nums: list, k: int) -> list:
     sorted_ocurrences = sorted(ocurrences.items(), key = lambda x: x[1], reverse = True)
 
     return list(dict(sorted_ocurrences).keys())[0:k]
+
+
+## Exercício 2
+
+def valid_input(s: str) -> bool:
+
+    stack = []
+
+    solution = None
+
+    for index, symbol in enumerate(s):
+
+        if symbol in ["(", "[", "{"]:
+
+            stack.append(symbol)
+
+        else: 
+        
+            if len(stack) > 0:          
+
+                if symbol is ")" and stack[0] is not "(":
+
+                    solution = False
+
+                    break
+
+                if symbol is "]" and stack[0] is not "[":
+
+                    solution = False
+
+                    break
+
+                if symbol is "}" and stack[0] is not "{":
+
+                    solution = False
+
+                    break
+
+            else:
+
+                solution = False
+
+    return solution
+
+valid_input("(){}[]")
+
