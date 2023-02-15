521b6f71763ef64fc19c11695152e380a01b1a68
Pedro Cavalcante Oliveira <pedrocolrj@gmail.com>
2023-02-15 19:37:51 -0300

    Update tarefa.py
    fiz um tokenizer e parser, implementando a computação da árvore

--- tarefa_final/tarefa.py
+++ tarefa_final/tarefa.py
@@ -10,14 +10,13 @@ def k_most_frequent(nums: list, k: int) -> list:
 
     return list(dict(sorted_ocurrences).keys())[0:k]
 
-
 ## Exercício 2
 
 def valid_input(s: str) -> bool:
 
     stack = []
 
-    solution = True
+    solution = True # válido até que se prove o contrário
 
     for symbol in s:
 
@@ -25,9 +24,9 @@ def valid_input(s: str) -> bool:
 
             stack.append(symbol)
 
-        else: 
+        else:
         
-            if len(stack) > 0:   
+            if len(stack) > 0:
 
                 # Nesse caso existe um elemento no topo da pilha que foi 
                 # aberto e testamos correspondência entre os dois
@@ -67,3 +66,56 @@ valid_input("(){}[]")
 valid_input("(](}")
 valid_input("[[[]]]")
 valid_input("[[[]])")
+
+# Exercício 3
+
+def tokenizer(s: str) -> list:
+
+    return [*s.strip()]
+
+tokenizer('3+2/4')
+
+def parser(s: str):
+
+    output = ["_"] * len(s) # cria uma lista do tamanho certo
+
+    for index, symbol in enumerate(s):
+
+        print(f"Index: {index}, symbol: {symbol}")
+
+        if symbol not in ['+', '-', '/', '*']:
+
+            output[index] = int(symbol)
+        
+        else:
+
+            output[index] = symbol
+
+    return output
+
+def computer(input: str) -> int:
+
+    if '*' in input:
+
+        position = input.find('*')
+
+        lhs = position - 1
+        rhs = position + 1
+
+        result = input[lhs] * input[rhs]
+
+        new_input = input
+        new_input[position] = result
+        
+        del new_input[lhs]
+        del new_input[rhs]
+
+        return computer(new_input)
+
+computer(parser(tokenizer("3*2+5")))
+
+
+
+
+
+
